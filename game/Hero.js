8b89e842bc757e6849d029e5eeb37b85b50b1c12
SimonChaw <chawblah@gmail.com>
2016-02-29 10:19:52 -0400

    Begin collision detection
    Animations working out.

--- game/Hero.js
+++ game/Hero.js
@@ -1,16 +1,21 @@
-var Hero = function(stage,assetManager){
+var Hero = function(stage,assetManager,ground){
     //init
-    var touchingDown = true;
+    var touchingDown = false;
     var alive = true;
     //keep track of scope
     var me = this;
+    var speed = 10;
+    var gravity = 0.5;
     
-    //custom event objects
+    //custom event oground[i]jects
     var eventOnDeath = new createjs.Event("onDeath",true);
-    
+    var shape = new createjs.Shape();
+    shape.type = "ground";
     //add hero to the world
     var sprite = assetManager.getSprite("assets");
+    shape.graphics.beginFill("").drawRect(sprite.x + 90, sprite.y + 160, 100, 20);
     stage.addChild(sprite);
+    stage.addChild(shape);
     console.log(sprite);
     //Move method.... implement movement class like Sean's example?
     
@@ -24,12 +29,47 @@ var Hero = function(stage,assetManager){
         return sprite;
     };
     
+    this.getSpeed = function(){
+        return speed;
+    }
     
     //-------- public methods
     this.init = function(){
         alive = true;
         //stage.addChild(me);
     };
+    
+    this.walkRight = function(){
+        sprite.x += speed;
+    }
+    
+    this.walkLeft = function(){
+        sprite.x -= speed;
+    }
+    
+    this.run = function(){
+        speed = 15;
+    }
+    
+    this.walk = function(){
+        speed = 10;
+    }
+    
+    
+    this.update = function(deltaTime){
+        checkIfGrounded();
+        console.log(touchingDown);
+        if(!touchingDown){
+            if(sprite.currentAnimation !== "boofAir"){
+                sprite.gotoAndPlay("boofAir");
+                console.log(sprite.currentAnimation);
+            }
+            //console.log(gravity * deltaTime);
+            sprite.y += gravity * deltaTime;
+        }
+        shape.x = sprite.x;
+        shape.y = sprite.y;
+    }
     
     this.kill = function(){
         if(alive){
@@ -45,5 +85,19 @@ var Hero = function(stage,assetManager){
         sprite.stop();
         e.remove();
         sprite.dispatchEvent(eventHeroKilled);
+    }
+    
+    function checkIfGrounded(){
+         if (
+        (shape.y == (ground[0].y)) ||
+        (shape.y > (ground[0].y + ground[0].height)) ||
+        ((shape.x + shape.width) < ground[0].x) ||
+        (shape.x > (ground[0].x + ground[0].width))
+    ){
+             touchingDown = true;
+         }else{
+             touchingDown = false;
+         }
+        console.log("Statement 1:"+ (shape.y) + " " + ground[0].y);
     }
 }
--- game/game.js
+++ game/game.js
@@ -14,9 +14,14 @@
     var upKey = false;
     var leftKey = false;
     var rightKey = false;
+    var shiftKey = false;
     // frame rate of game
     var frameRate = 26;
     var Boof;
+    var ground = Array();
+    
+    var lastUpdate;
+    var myInterval;
     // game objects
     var assetManager;
     // ------------------------------------------------------------ event handlers
@@ -43,6 +48,12 @@
         // kill event listener
         stage.removeEventListener("onAllAssetsLoaded", onSetup);
 
+        //Alternatively use can also use the graphics property of the Shape class to renderer the same as above.
+        var shape = new createjs.Shape();
+        shape.type = "ground";
+        shape.graphics.beginFill("#ff0000").drawRect(0, 500, 500, 100);
+        
+        stage.addChild(shape);
         // construct game objects
         //background = assetManager.getSprite("assets");
         //background.gotoAndStop("background");
@@ -62,11 +73,19 @@
         //userInterface = new UserInterface(stage, assetManager, snakeMaxSpeed);
         //userInterface.setupMe();	
         
-        Boof = new Hero(stage, assetManager);
-        Boof.sprite = Boof.getSprite();
+        
         // startup the ticker
         createjs.Ticker.setFPS(frameRate);
         createjs.Ticker.addEventListener("tick", onTick);
+        lastUpdate = Date.now();
+        for(var i = 0;i <  stage.children.length; i ++){
+            if(stage.children[i].type == "ground"){
+                ground.push(stage.children[i]);
+            }
+        }
+        Boof = new Hero(stage, assetManager,ground);
+        Boof.sprite = Boof.getSprite();
+        //myInterval = setInterval(onTick,0);
         onStartGame();
     }
 
@@ -105,10 +124,27 @@
 
     function onKeyDown(e) {
         // which keystroke is down?
-        if (e.keyCode == 37) leftKey = true;
-        else if (e.keyCode == 39) rightKey = true;
-        else if (e.keyCode == 38) upKey = true;
-        else if (e.keyCode == 40) downKey = true;
+        if (e.keyCode == 37 && !leftKey){
+            leftKey = true;
+            Boof.sprite.regX = 300;
+            Boof.sprite.scaleX = -1;
+            Boof.sprite.gotoAndPlay("boofWalk");
+        }else if(e.keyCode == 39 && !rightKey){
+            rightKey = true;
+            Boof.sprite.regX = 0;
+            Boof.sprite.scaleX = 1;
+            Boof.sprite.gotoAndPlay("boofWalk");
+        }else if (e.keyCode == 38 && !upKey){
+            upKey = true;
+            Boof.sprite.gotoAndPlay("boofJump");
+        }
+        else if (e.keyCode == 40){
+            downKey = true;
+        }else if(e.keyCode == 16){
+            shiftKey = true;
+        }
+        
+        //console.log(e.keyCode);
     }
 
     function onKeyUp(e) {
@@ -117,32 +153,42 @@
         else if (e.keyCode == 39) rightKey = false;
         else if (e.keyCode == 38) upKey = false;
         else if (e.keyCode == 40) downKey = false;
+        else if (e.keyCode == 16) shiftKey = false;
     }
 
     function onTick(e) {
         // TESTING FPS
         document.getElementById("fps").innerHTML = createjs.Ticker.getMeasuredFPS();
-
+        
+        var now = Date.now();
+        var deltaTime = now - lastUpdate;
+        lastUpdate = now;
+        
         
         // only monitor keyboard if snake is alive
         if (Boof.isAlive()) {
+            if(shiftKey){
+                Boof.run();
+            }else{
+                Boof.walk();
+            }
+            
+            
             if (leftKey) {
-                Boof.sprite.regX = 300;
-                Boof.sprite.scaleX = -1;
+                Boof.walkLeft();
             } else if (rightKey) {
-                Boof.sprite.regX = 0;
-                Boof.sprite.scaleX = 1;
+                Boof.walkRight();
             } else if (upKey) {
                 //console.log("jumping");
             } else if (downKey) {
-                Boof.sprite.gotoAndPlay("boofWalk");
+                
                 //console.log("croutching I guess");
             } else {
                 //console.log("here");
             }
         }
         
-
+        Boof.update(deltaTime);
         // update the stage!
         stage.update();
     }
